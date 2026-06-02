8c7e141b5524fd6cb76ab7ced195937e85ea6e79
Tanya Garg <162972888+Tanya-garg10@users.noreply.github.com>
2026-06-02 09:28:41 +0530

    Time: 4 ms (30.84%), Space: 19.3 MB (48.51%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        
+        a = cost[0]
+        b = cost[1]
+        
+        for i in range(2, len(cost)):
+            a, b = b, cost[i] + min(a, b)
+        
+        return min(a, b)
